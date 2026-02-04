b4295e5241b738f84f05fbf960e2ddfdd9e1acd2
Bhavanish Reddy <bhavanishreddy@gmail.com>
2026-02-04 11:32:53 +0000

    added actions and functions additional to curd aoperations

--- srv/CatalogService.cds
+++ srv/CatalogService.cds
@@ -1,10 +1,14 @@
 using{anubhav.db.master, anubhav.db.transaction} from '../db/data-model';
 
-service CatalogService{
+service CatalogService @(path:'CatalogService'){
     entity BusinessPartnerSet as projection on master.businesspartner;
     entity AddressSet as projection on master.address;
     entity EmployeeSet as projection on master.employees;
     entity ProductSet as projection on master.product;
-    entity POs as projection on transaction.purchaseorder;
+    entity POs as projection on transaction.purchaseorder
+    actions{
+        action boost() returns POs;
+        function largestOrder() returns POs;
+    };
     entity POItems as projection on transaction.poitems;
 }
---
+++ srv/CatalogService.js
@@ -0,0 +1,39 @@
+module.exports = cds.service.impl(async function(){
+
+    const {POs} = this.entities;
+
+    this.on('boost', async(req)=>{
+        console.log('aa gaya');
+       
+           try {
+            const ID = req.params[0];
+             //start a db transaction
+            const tx = cds.tx(req);
+            //CDS Query Language - communicate to DB in agnostic manner
+
+            await tx.update(POs).with({
+                GROSS_AMOUNT:{ '+=' :2000}
+            }).where(ID);
+           } 
+           catch (error) {
+            return "Error"+ error.toString();
+           }
+    });
+
+    this.on('largestOrder', async(req)=>{
+        console.log('aa gaya');
+       
+           try {
+            //start a new DB transaction
+            const tx = cds.tx(req);
+            //Read the PO with the highest gross amount
+            //Read orders desc by gross + get the first record
+            const recordData = tx.read(POs).orderBy({
+                GROSS_AMOUNT: 'desc'
+            }).limit(1);
+           } 
+           catch (error) {
+            return "Error"+ error.toString();
+           }
+    });
+})
